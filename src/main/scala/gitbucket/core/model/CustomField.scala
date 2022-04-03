94581a92256b9ff9b13872d465a347e2ebd02b00
Naoki Takezoe <takezoe@gmail.com>
2022-04-03 14:22:32 +0900

    Add tables and models for custom fields

---
+++ src/main/scala/gitbucket/core/model/CustomField.scala
@@ -0,0 +1,31 @@
+package gitbucket.core.model
+
+trait CustomFieldComponent extends TemplateComponent { self: Profile =>
+  import profile.api._
+
+  lazy val CustomFields = TableQuery[CustomFields]
+
+  class CustomFields(tag: Tag) extends Table[CustomField](tag, "CUSTOM_FIELD_DEFINITION") with BasicTemplate {
+    val fieldId = column[Int]("FIELD_ID", O AutoInc)
+    val fieldName = column[String]("FIELD_NAME")
+    val fieldType = column[String]("FIELD_TYPE")
+    val enableForIssues = column[Boolean]("ENABLE_FOR_ISSUES")
+    val enableForPullRequests = column[Boolean]("ENABLE_FOR_PULL_REQUESTS")
+    def * =
+      (userName, repositoryName, fieldId, fieldName, fieldType, enableForIssues, enableForPullRequests)
+        .<>(CustomField.tupled, CustomField.unapply)
+
+    def byPrimaryKey(userName: String, repositoryName: String, fieldId: Int) =
+      (this.userName === userName.bind) && (this.repositoryName === repositoryName.bind) && (this.fieldId === fieldId.bind)
+  }
+}
+
+case class CustomField(
+  userName: String,
+  repositoryName: String,
+  fieldId: Int,
+  fieldName: String,
+  fieldType: String, // LONG, STRING, BOOLEAN or DATE
+  enableForIssues: Boolean,
+  enableForPullRequests: Boolean
+)
---
+++ src/main/scala/gitbucket/core/model/IssueCustomFields.scala
@@ -0,0 +1,33 @@
+package gitbucket.core.model
+
+trait IssueCustomFieldComponent extends TemplateComponent { self: Profile =>
+  import profile.api._
+  import self._
+
+  lazy val IssueCustomFields = TableQuery[IssueCustomFields]
+
+  class IssueCustomFields(tag: Tag) extends Table[IssueCustomField](tag, "ISSUE_CUSTOM_FIELD") with IssueTemplate {
+    val fieldId = column[Int]("FIELD_ID")
+    val longValue = column[Option[Long]]("LONG_VALUE")
+    val stringValue = column[Option[String]]("STRING_VALUE")
+    val booleanValue = column[Option[Boolean]]("BOOLEAN_VALUE")
+    val dateValue = column[Option[java.util.Date]]("DATE_VALUE")
+    def * =
+      (userName, repositoryName, issueId, fieldId, longValue, stringValue, booleanValue, dateValue)
+        .<>(IssueCustomField.tupled, IssueCustomField.unapply)
+
+    def byPrimaryKey(owner: String, repository: String, issueId: Int, fieldId: Int) =
+      byIssue(owner, repository, issueId) && (this.fieldId === fieldId.bind)
+  }
+}
+
+case class IssueCustomField(
+  userName: String,
+  repositoryName: String,
+  issueId: Int,
+  fieldId: Int,
+  longValue: Option[Long],
+  stringValue: Option[String],
+  booleanValue: Option[Boolean],
+  dateValue: Option[java.util.Date]
+)
